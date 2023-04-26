4273d7f67499747bfc44bd5e206543d68c3134bb
Jon Webb <webbja123@gmail.com>
2023-04-26 08:00:30 -0600

    Initial commit

---
+++ finance_db/database.py
@@ -0,0 +1,22 @@
+# Import necessary packages here
+
+# - If a package and a module within the package is to be imported
+#   uncomment the following lines where dir is the directory containing
+#   the source files.  These lines should go above the module imports
+# import sys
+# sys.path.insert(1, os.path.abspath(dir))
+# ==========================================================================================
+# ==========================================================================================
+
+# File:    main.py
+# Date:    April 26, 2023
+# Author:  Jonathan A. Webb
+# Purpose: This file contains functions that will create a database, add, subtract, and
+#          modify data in the database
+# ==========================================================================================
+# ==========================================================================================
+# Insert Code here
+
+# ==========================================================================================
+# ==========================================================================================
+# eof
