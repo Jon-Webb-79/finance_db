ee5ec72bf98c305f6f64230862850fac63521c3f
Jon Webb <webbja123@gmail.com>
2023-04-26 22:23:20 -0600

    Added sphinx documentation for create_database function

---
+++ docs/sphinx/source/Database.rst
@@ -0,0 +1,7 @@
+********
+Database
+********
+The **database.py** file contains functions that are used to build and maintain a database.
+The following list describes the functions within this module.
+
+.. autofunction:: finance_db.database.create_database
--- finance_db/database.py
+++ finance_db/database.py
@@ -23,6 +23,71 @@ import sys
 
 
 def create_database(file_name: str) -> None:
+    """
+    :param file_name: The name of the database to be created without a .db file
+                      extension
+
+    This function will create a SQLite table for expenses and another table
+    for sales.  The tables have the following schema.
+
+    Expenses Table:
+
+    +---------------+------------------+--------------------------------------+
+    | Column        | Data Type        | Description                          |
+    +===============+==================+======================================+
+    | id            | INTEGER PRIMARY  | Unique identifier for the expense    |
+    |               | KEY              |                                      |
+    +---------------+------------------+--------------------------------------+
+    | date          | DATE NOT NULL    | Date of the expense                  |
+    +---------------+------------------+--------------------------------------+
+    | time          | TIME NOT NULL    | Time of the expense                  |
+    +---------------+------------------+--------------------------------------+
+    | expense_value | REAL NOT NULL    | Value of the expense                 |
+    +---------------+------------------+--------------------------------------+
+    | company       | TEXT NOT NULL    | Company associated with the expense  |
+    +---------------+------------------+--------------------------------------+
+    | description   | TEXT NOT NULL    | Description of the expense           |
+    +---------------+------------------+--------------------------------------+
+    | modified_date | DATE NOT NULL    | Date when the expense was last       |
+    |               |                  | modified. (default: current date)    |
+    +---------------+------------------+--------------------------------------+
+    | modified_time | TIME NOT NULL    | Time when the expense was last       |
+    |               |                  | modified. (default: current time)    |
+    +---------------+------------------+--------------------------------------+
+
+    Sales Table:
+
+    +---------------+------------------+---------------------------------------+
+    | Column        | Data Type        | Description                           |
+    +===============+==================+=======================================+
+    | id            | INTEGER PRIMARY  | Unique identifier for the sale        |
+    |               | KEY              |                                       |
+    +---------------+------------------+---------------------------------------+
+    | date          | DATE NOT NULL    | Date of the sale                      |
+    +---------------+------------------+---------------------------------------+
+    | time          | TIME NOT NULL    | Time of the sale                      |
+    +---------------+------------------+---------------------------------------+
+    | first_name    | TEXT NOT NULL    | First name of the customer            |
+    +---------------+------------------+---------------------------------------+
+    | last_name     | TEXT NOT NULL    | Last name of the customer             |
+    +---------------+------------------+---------------------------------------+
+    | email_address | TEXT NOT NULL    | Email address of the customer         |
+    +---------------+------------------+---------------------------------------+
+    | phone_number  | TEXT             | Phone number of the customer          |
+    |               |                  | (optional)                            |
+    +---------------+------------------+---------------------------------------+
+    | product_id    | INTEGER NOT NULL | Unique identifier for the product     |
+    +---------------+------------------+---------------------------------------+
+    | modified_date | DATE NOT NULL    | Date when the sale was last modified. |
+    |               |                  | (default: current date)               |
+    +---------------+------------------+---------------------------------------+
+    | modified_time | TIME NOT NULL    | Time when the sale was last modified. |
+    |               |                  | (default: current time)               |
+    +---------------+------------------+---------------------------------------+
+
+    The modified_date and modified_time fields are automatically updated
+    to the current date and time whenever a row is inserted or updated.
+    """
     # append .db extension to file name
     file_name = file_name + ".db"
 
